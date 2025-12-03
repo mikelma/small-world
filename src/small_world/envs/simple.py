b40f2fc27252d135ece4a0d6f020d1994fc7d796
Mikel <mikelma@tutanota.com>
2025-12-03 11:33:28 +0100

    initial commit

---
+++ src/small_world/envs/simple.py
@@ -0,0 +1,64 @@
+import jax
+import jax.numpy as jnp
+from typing import Any
+from jaxtyping import Array, Scalar, Integer
+
+from ..environment import Environment, EnvParams, State, EnvCarry, Timestep
+
+
+class SimpleEnvCarry(EnvCarry): ...
+
+
+class Simple(Environment):
+    def default_params(self, **kwargs: dict[str, Any]):
+        params = EnvParams(
+            height=10, width=10, view_size=5, num_agents=1, num_actions=4
+        )
+        params = params.replace(**kwargs)
+        return params
+
+    def _generate_problem(self, params: EnvParams, key: jax.Array) -> State:
+        # grid = jnp.zeros((params.height, params.width))
+        grid = (
+            jnp.arange(params.height * params.width, dtype=float).reshape(
+                params.height, params.width
+            )
+            / 10
+        )
+
+        return State(
+            grid=grid,
+            step=0,
+            agents_pos=jnp.asarray((5, 5), dtype=int).reshape(1, -1),
+            carry=SimpleEnvCarry(),
+        )
+
+    def _update(
+        self,
+        params: EnvParams,
+        timestep: Timestep,
+        actions: Integer[Scalar, "{params.num_agents}"],
+        positions: Integer[Array, "{params.num_agents} 2"],
+        new_positions: Integer[Array, "{params.num_agents} 2"],
+    ) -> Timestep:
+        # 1) update state
+        # simple update: update with new positions and advance step by one, leave the grid unchanged
+        prev_state = timestep.state
+        new_state = prev_state.replace(
+            grid=prev_state.grid,
+            step=timestep.state.step + 1,
+            agents_pos=new_positions,
+        )
+
+        # update observations
+
+        # update reward
+        new_rewards = timestep.rewards  # constant reward
+
+        return Timestep(
+            timestep.observations,
+            new_rewards,
+            new_state,
+        )
+
+        pass
